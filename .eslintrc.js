ce6404388076231377c47f7f6f0bf1dc79968b6d
Thomas Amsler <tamsler@infocentric.ch>
2021-06-25 11:32:57 +0200

    update type check and linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
 		'plugin:prettier/recommended',
 		'plugin:nuxt/recommended',
 	],
+	ignorePatterns: ['**/generated/*'],
 	plugins: [],
 	// add your custom rules here
 	rules: {},
